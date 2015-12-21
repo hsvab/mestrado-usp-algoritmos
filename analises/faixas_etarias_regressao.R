adc72f471c97c912601871af52e8fb62025a9bd3
Haydee Svab <haydee.svab@gmail.com>
2015-12-21 00:37:19 -0200

    Regressoes que relacionam o total de viagens com estrutura familiar

--- analises/faixas_etarias_regressao.R
+++ analises/faixas_etarias_regressao.R
@@ -36,3 +36,15 @@ modelo_3 <- lm(TOT_VIAG ~ FX_ET_0 + FX_ET_1 + FX_ET_2 + FX_ET_3 + FX_ET_4 + FX_E
                  FX_ET_8 + FX_ET_9 + FX_ET_10 + FX_ET_11 + FX_ET_12 + FX_ET_13 + FX_ET_14 + FX_ET_15 +
                  FX_ET_16 + FX_ET_17 + FX_ET_18 + FX_ET_19 + FX_ET_20, data=od_reg_3)
 summary(modelo_3)
+
+
+# od_reg_1 é o banco de dados usado para regressão, filtrado por:
+# pessoa (F_PESS=1)
+# mulher (SEXO==2)
+# pessoa responsável pela família (SIT_FAM==1)
+od_reg_4 <- od %>% filter(SEXO==1, F_PESS==1, SIT_FAM==2)
+
+# Regressão linear múltipla de TOT_VIAG de od_reg com as faixas etárias
+modelo_4 <- lm(TOT_VIAG ~ PRESENCA_FILH_ate4 + PRESENCA_FILH_5a9 + 
+                 PRESENCA_FILH_10a14 + PRESENCA_FILH_15a19 + PRESENCA_IDOSO, data=od_reg_4)
+summary(modelo_4)
