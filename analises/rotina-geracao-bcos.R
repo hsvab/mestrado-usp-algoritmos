d19ab871eb042de35abe171ec10f325bf030003a
Haydee Svab <haydee.svab@gmail.com>
2016-01-09 22:53:15 -0200

    Rotina de geração dos bancos de dados

--- analises/rotina-geracao-bcos.R
+++ analises/rotina-geracao-bcos.R
@@ -1,56 +1,70 @@
 # Lê a base od processada e unificada
-od <- read.csv(file = "/home/haydee/Área de trabalho/mestrado-PTR/OD/Consolidado/banco unico - pols/od.csv",
-               header = TRUE, sep = ";", dec = ",", colClasses = c("ID_DOM"="character", "ID_FAM"="character", "ID_PESS"="character", "ID_VIAG"="character"))
+library(ggplot2)
+library(readstata13)
+library(Hmisc)
+library(psych)
+library(plyr)
+library(dplyr)
 
-# Gera od-2.csv
+od <- read.csv(file = "/home/haydee/Área de trabalho/mestrado-PTR/OD/Consolidado/banco unico - pols/od.csv",
+               header = TRUE, sep = ";", dec = ",", colClasses = c("ID_DOM"="character", "ID_FAM"="character",
+                                                                   "ID_PESS"="character", "ID_VIAG"="character"))
+
+# Gera od-01.csv
+source('correcao_tipo_est_auto.R')
+source('correcao_serv_pas.R')
+od %>% write.table(file = "/home/haydee/Área de trabalho/mestrado-PTR/OD/Consolidado/banco unico - pols/od-01.csv", 
+                   sep = ";", dec = ",", row.names = FALSE)
+
+# Gera od-02.csv
 source('faixas_etarias.R')
 source('faixas_horarias.R')
 source('faixas_renda.R')
-od %>% write.table(file = "/home/haydee/Área de trabalho/mestrado-PTR/OD/Consolidado/banco unico - pols/od-2.csv", 
+od %>% write.table(file = "/home/haydee/Área de trabalho/mestrado-PTR/OD/Consolidado/banco unico - pols/od-02.csv", 
                    sep = ";", dec = ",", row.names = FALSE)
 
-# Gera od-3.csv
+# Gera od-03.csv
 source('contagem_modos_viagem.R')
-od %>% write.table(file = "/home/haydee/Área de trabalho/mestrado-PTR/OD/Consolidado/banco unico - pols/od-3.csv", 
+od %>% write.table(file = "/home/haydee/Área de trabalho/mestrado-PTR/OD/Consolidado/banco unico - pols/od-03.csv", 
                    sep = ";", dec = ",", row.names = FALSE)
 
-# Gera od-4.csv
+# Gera od-04.csv
 source('distancias_pessoa.R')
-od %>% write.table(file = "/home/haydee/Área de trabalho/mestrado-PTR/OD/Consolidado/banco unico - pols/od-4.csv", 
+od %>% write.table(file = "/home/haydee/Área de trabalho/mestrado-PTR/OD/Consolidado/banco unico - pols/od-04.csv", 
                    sep = ";", dec = ",", row.names = FALSE)
 rm(AUX1, AUX2, AUX3)
 
-# Gera od-5.csv
+# Gera od-05.csv
 source('duracoes_pessoas.R')
-od %>% write.table(file = "/home/haydee/Área de trabalho/mestrado-PTR/OD/Consolidado/banco unico - pols/od-5.csv", 
+od %>% write.table(file = "/home/haydee/Área de trabalho/mestrado-PTR/OD/Consolidado/banco unico - pols/od-05.csv", 
                    sep = ";", dec = ",", row.names = FALSE)
 rm(AUX1, AUX2, AUX3)
 
-# Gera od-6.csv
+# Gera od-06.csv
 source('contagem_modos_pessoa.R')
-od %>% write.table(file = "/home/haydee/Área de trabalho/mestrado-PTR/OD/Consolidado/banco unico - pols/od-6.csv", 
+od %>% write.table(file = "/home/haydee/Área de trabalho/mestrado-PTR/OD/Consolidado/banco unico - pols/od-06.csv", 
                    sep = ";", dec = ",", row.names = FALSE)
 rm(AUX1)
 
-# Gera od-7.csv
+# Gera od-07.csv
 source('contagem_motivos_viagem.R')
 source('contagem_motivos_pessoas.R')
-od %>% write.table(file = "/home/haydee/Área de trabalho/mestrado-PTR/OD/Consolidado/banco unico - pols/od-7.csv", 
+od %>% write.table(file = "/home/haydee/Área de trabalho/mestrado-PTR/OD/Consolidado/banco unico - pols/od-07.csv", 
                    sep = ";", dec = ",", row.names = FALSE)
 rm(AUX1)
 
-# Gera od-8.csv
+# Gera od-08.csv
 source('contagem_periodos_viagens.R')
 source('contagem_periodos_pessoas.R')
-od %>% write.table(file = "/home/haydee/Área de trabalho/mestrado-PTR/OD/Consolidado/banco unico - pols/od-8.csv", 
+od %>% write.table(file = "/home/haydee/Área de trabalho/mestrado-PTR/OD/Consolidado/banco unico - pols/od-08.csv", 
                    sep = ";", dec = ",", row.names = FALSE)
 rm(AUX1)
 
-# Gera od-9.csv
+# Gera od-09.csv
 source('dummies_fxs_etarias.R')
 source('correcao_faixas_etarias.R')
 source('presenca_idoso_crianca.R')
-od %>% write.table(file = "/home/haydee/Área de trabalho/mestrado-PTR/OD/Consolidado/banco unico - pols/od-9.csv", 
+od %>% write.table(file = "/home/haydee/Área de trabalho/mestrado-PTR/OD/Consolidado/banco unico - pols/od-09.csv", 
                    sep = ";", dec = ",", row.names = FALSE)
 rm(AUX1)
 
@@ -58,5 +72,11 @@ rm(AUX1)
 source('dummies_anos.R')
 source('presenca_carro.R')
 od %>% write.table(file = "/home/haydee/Área de trabalho/mestrado-PTR/OD/Consolidado/banco unico - pols/od-10.csv", 
+                   sep = ";", dec = ",", row.names = FALSE)
+rm(AUX1)
+
+# Gera od-11.csv
+source('contagem_pessoas_familia.R')
+od %>% write.table(file = "/home/haydee/Área de trabalho/mestrado-PTR/OD/Consolidado/banco unico - pols/od-11.csv", 
                    sep = ";", dec = ",", row.names = FALSE)
 rm(AUX1)
