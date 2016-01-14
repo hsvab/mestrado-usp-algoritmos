624efdd823d88ea7ec431cd99ef95bbec589f496
Diego Rabatone Oliveira <diraol@diraol.eng.br>
2016-01-14 16:37:07 -0200

    Acertando função auxiliar de bibliotecas para instalar caso ela não exista

--- R/inicializacao.R
+++ R/inicializacao.R
@@ -1,4 +1,22 @@
 cat("Seja bem vinda(o) ao Projeto do Mestrado da Haydée.\n")
+packages<-function(x){
+  x<-as.character(x)
+  base::require(x,character.only=TRUE, quietly=TRUE)
+}
+
+pacotes_mestrado <- c("ggplot2", "readstata13", "foreign","data.table",
+                      "Hmisc", "gmodels", "memisc", "sfsmisc", "lazyeval",
+                       "psych", "plyr", "fastcluster", "dplyr")
+
+cat("Carregando as bibliotecas:\n")
+cat(pacotes_mestrado,"\n")
+
+for( pacote in pacotes_mestrado ) {
+  packages(pacote)
+}
+
+rm(packages, pacotes_mestrado, pacote)
+
 cat("Carregando as funções auxiliares.....")
 source('R/funcoes_auxiliares.R')
 cat(" OK\n")
@@ -11,7 +29,7 @@ cat(" OK\n")
 cat("Carregando funções que aplica labels no dataframe da OD....")
 source('R/aplica_labels.R')
 cat(" OK\n")
-carrega_bibliotecas()
 cat("\nAlterando o 'working directory' para 'analises'\n")
 setwd('analises')
-cat("\nAmbiente pronto para ser utilizado, bom trabalho! <3")
+cat("\nTerminando de carregar o workspace, assim que ele estiver carregado\n",
+    "o ambiente estará pronto para ser utilizado, bom trabalho! <3\n")
