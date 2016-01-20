96a242c3b49027be5d598146532f1384e0701311
Haydee Svab <haydee.svab@gmail.com>
2016-01-20 16:41:03 -0200

    Corrigindo script de contagem de períodos

--- analises/contagem_periodos_viagens.R
+++ analises/contagem_periodos_viagens.R
@@ -1,5 +1,5 @@
 # Criando novas variáveis com contagem (dummy) dos periodos (marcados pelo horário de chegada), por viagem
-# 
+#
 # 0 -> não fez viagem
 # VIAG_PER_MADRUG é marcador da viagem que terminou entre 0h01 e 5h00
 # VIAG_PER_COM_MAN é marcador da viagem que terminou entre 5h01 e 9h00
@@ -10,7 +10,7 @@
 # VIAG_PER_NOITE é marcador da viagem que terminou 22h01 e 0h
 
 od <- od  %>%
-  
+
   # Inicializando as variáveis de PERIODO para VIAGENS
   mutate(VIAG_PER_MADRUG=0) %>%
   mutate(VIAG_PER_COM_MAN=0) %>%
@@ -26,52 +26,52 @@ od <- od  %>%
     (FAIXA_HORARIA==1 & !is.na(FAIXA_HORARIA)),
     VIAG_PER_MADRUG+1,
     VIAG_PER_MADRUG
-  )) 
+  ))
 
 od <- od  %>%
   # Analisando VIAG_PER_COM_MAN
   mutate(VIAG_PER_COM_MAN=ifelse(
-    (FAIXA_HORARIA==1 & !is.na(FAIXA_HORARIA)),
+    (FAIXA_HORARIA==2 & !is.na(FAIXA_HORARIA)),
     VIAG_PER_COM_MAN+1,
     VIAG_PER_COM_MAN
-  )) 
+  ))
 
 od <- od  %>%
   # Analisando VIAG_PER_MANHA
   mutate(VIAG_PER_MANHA=ifelse(
-    (FAIXA_HORARIA==1 & !is.na(FAIXA_HORARIA)),
+    (FAIXA_HORARIA==3 & !is.na(FAIXA_HORARIA)),
     VIAG_PER_MANHA+1,
     VIAG_PER_MANHA
-  )) 
+  ))
 
 od <- od  %>%
   # Analisando VIAG_PER_MEIODIA
   mutate(VIAG_PER_MEIODIA=ifelse(
-    (FAIXA_HORARIA==1 & !is.na(FAIXA_HORARIA)),
+    (FAIXA_HORARIA==4 & !is.na(FAIXA_HORARIA)),
     VIAG_PER_MEIODIA+1,
     VIAG_PER_MEIODIA
-  )) 
+  ))
 
 od <- od  %>%
   # Analisando VIAG_PER_TARDE
   mutate(VIAG_PER_TARDE=ifelse(
-    (FAIXA_HORARIA==1 & !is.na(FAIXA_HORARIA)),
+    (FAIXA_HORARIA==5 & !is.na(FAIXA_HORARIA)),
     VIAG_PER_TARDE+1,
     VIAG_PER_TARDE
-  )) 
+  ))
 
 od <- od  %>%
   # Analisando VIAG_PER_COM_NOI
   mutate(VIAG_PER_COM_NOI=ifelse(
-    (FAIXA_HORARIA==1 & !is.na(FAIXA_HORARIA)),
+    (FAIXA_HORARIA==6 & !is.na(FAIXA_HORARIA)),
     VIAG_PER_COM_NOI+1,
     VIAG_PER_COM_NOI
-  )) 
+  ))
 
 od <- od  %>%
   # Analisando VIAG_PER_NOITE
   mutate(VIAG_PER_NOITE=ifelse(
-    (FAIXA_HORARIA==1 & !is.na(FAIXA_HORARIA)),
+    (FAIXA_HORARIA==7 & !is.na(FAIXA_HORARIA)),
     VIAG_PER_NOITE+1,
     VIAG_PER_NOITE
-  )) 
+  ))
